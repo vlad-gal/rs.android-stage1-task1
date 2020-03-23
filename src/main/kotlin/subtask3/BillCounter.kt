57620e7af09cd8844727a176a824513e6d3e769d
vlad-gal <vlgalathevich@gmail.com>
2020-03-23 13:01:52 +0300

    feat: implement calculateFairlySplit function

--- src/main/kotlin/subtask3/BillCounter.kt
+++ src/main/kotlin/subtask3/BillCounter.kt
@@ -3,7 +3,7 @@ package subtask3
 class BillCounter {
 
     // TODO: Complete the following function
-    // The output could be "Bon Appetit" or the string with number(e.g "10")
+
     fun calculateFairlySplit(bill: IntArray, k: Int, b: Int): String {
         return if ((bill.sum()-bill[k])/2 == b){
             "Bon Appetit"
