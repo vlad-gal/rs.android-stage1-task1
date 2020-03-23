ba0fd7c09a48ab5f668a2dd45288864ae0a20322
vlad-gal <vlgalathevich@gmail.com>
2020-03-23 12:56:10 +0300

    feat: implement getResult function

--- src/main/kotlin/subtask2/MiniMaxSum.kt
+++ src/main/kotlin/subtask2/MiniMaxSum.kt
@@ -4,6 +4,6 @@ class MiniMaxSum {
 
     // TODO: Complete the following function
     fun getResult(input: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        return arrayOf(input.sum()- input.max()!!,input.sum()- input.min()!!).toIntArray()
     }
 }
