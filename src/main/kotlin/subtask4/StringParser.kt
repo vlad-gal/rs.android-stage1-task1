6a9e8595a9ab90e460386ab72d7720fe53e70a51
vlad-gal <vlgalathevich@gmail.com>
2020-03-23 13:02:39 +0300

    feat: implement getResult function

--- src/main/kotlin/subtask4/StringParser.kt
+++ src/main/kotlin/subtask4/StringParser.kt
@@ -4,6 +4,38 @@ class StringParser {
 
     // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        val p1 = '<' to '>'
+        val p2 = '(' to ')'
+        val p3 = '[' to ']'
+        val parseStrings = arrayListOf<String>()
+
+
+        for (i in inputString.indices){
+            when {
+                inputString[i] == p1.first ->  parseStrings.add(getString(inputString.substring(i),p1))
+                inputString[i] == p2.first ->  parseStrings.add(getString(inputString.substring(i),p2))
+                inputString[i] == p3.first ->  parseStrings.add(getString(inputString.substring(i),p3))
+
+            }
+        }
+
+        return parseStrings.toTypedArray()
     }
+
+    private fun getString(subString: String, pair: Pair<Char,Char>): String {
+        var count = 0
+        var result = ""
+        for (i in 1 until subString.length) {
+            if (subString[i] == pair.first) {
+                count++
+            } else if (subString[i] == pair.second && count != 0) {
+                count--
+            } else if (subString[i] == pair.second && count == 0) {
+                result = subString.substring(1, i)
+                break
+            }
+        }
+        return result
+    }
+
 }
